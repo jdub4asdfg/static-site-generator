b9638e8837ae0542b9729bbe4ad3783c7848ccf4
jdub4asdfg <junweilee42@gmail.com>
2025-05-25 06:25:43 +0800

    Added a method to TextNode that turns a text node into a html node with the corresponding tag, value and url

--- src/textnode.py
+++ src/textnode.py
@@ -1,22 +1,46 @@
 from enum import Enum
+from htmlnode import LeafNode
+
 
 class TextType(Enum):
     NORMAL = 1
     BOLD = 2
     ITALIC = 3
     CODE = 4
-    LINKS = 5
-    IMAGES = 6
+    LINK = 5
+    IMAGE = 6
+
 
 class TextNode:
-    def __init__(self, text, text_type, url = None):
+    def __init__(self, text, text_type, url=None):
         self.text = text
         self.text_type = text_type
         self.url = url
 
     def __eq__(self, other):
-        if self.text == other.text and self.text_type == other.text_type and self.url == other.url:
+        if (
+            self.text == other.text
+            and self.text_type == other.text_type
+            and self.url == other.url
+        ):
             return True
-    
+
     def __repr__(self):
-        return f'TextNode({self.text}, {self.text_type}, {self.url})'
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
+
+    def text_to_html_node(self):
+        match self.text_type:
+            case TextType.NORMAL:
+                return LeafNode(None, self.text)
+            case TextType.BOLD:
+                return LeafNode("b", self.text)
+            case TextType.ITALIC:
+                return LeafNode("i", self.text)
+            case TextType.CODE:
+                return LeafNode("code", self.text)
+            case TextType.LINK:
+                return LeafNode("a", self.text, {"href": self.url})
+            case TextType.IMAGE:
+                return LeafNode("img", "", {"src": self.url, "alt": self.text})
+            case _:
+                raise Exception
