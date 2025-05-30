05008ff7b42978cbf873aa508cde84b88650f8c3
jdub4asdfg <junweilee42@gmail.com>
2025-05-30 10:10:31 +0800

    GitHub Pages compatible

--- src/generator.py
+++ src/generator.py
@@ -3,7 +3,7 @@ import re
 from functions import markdown_to_html_nodes
 
 
-def recursive_page_generation(dir_path_content, template_path, dest_dir_path):
+def recursive_page_generation(dir_path_content, template_path, dest_dir_path, basepath):
     list_of_files = os.listdir(dir_path_content)
     for file in list_of_files:
         new_dir_path_content = os.path.join(dir_path_content, file)
@@ -11,21 +11,25 @@ def recursive_page_generation(dir_path_content, template_path, dest_dir_path):
             new_dest_dir_path = os.path.join(dest_dir_path, file)
             os.mkdir(new_dest_dir_path)
             recursive_page_generation(
-                new_dir_path_content, template_path, new_dest_dir_path
+                new_dir_path_content, template_path, new_dest_dir_path, basepath
             )
         elif os.path.isfile(new_dir_path_content):
             html_file_name = re.findall(r"^(.*)md", file)[0] + "html"
             new_dest_dir_path = os.path.join(dest_dir_path, html_file_name)
-            generate_page(new_dir_path_content, template_path, new_dest_dir_path)
+            generate_page(
+                new_dir_path_content, template_path, new_dest_dir_path, basepath
+            )
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     index_content = get_content(from_path)
     template_content = get_content(template_path)
     html_string = markdown_to_html_nodes(index_content).to_html()
     title = extract_title(index_content)
     website_content = template_content.replace("{{ Title }}", title)
     website_content = website_content.replace("{{ Content }}", html_string)
+    website_content = website_content.replace('href="/', f'href="{basepath}')
+    website_content = website_content.replace('src="/', f'src="{basepath}')
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "":
         os.makedirs(dest_dir_path, exist_ok=True)
--- src/main.py
+++ src/main.py
@@ -1,28 +1,35 @@
 import os
+import sys
 import shutil
 
 from copystatic import recursive_copying
 from generator import recursive_page_generation
 
 dir_path_static = "./static"
-dir_path_public = "./public"
+dir_path_docs = "./docs"
 
 dir_path_content = "./content"
 template_path = "./template.html"
-
+default_basepath = '/'
 
 def main():
-    print("Deleting public directory...")
-    if os.path.exists(dir_path_public):
-        shutil.rmtree(dir_path_public)
+    basepath = default_basepath
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+
+    print("Deleting docs directory...")
+    if os.path.exists(dir_path_docs):
+        shutil.rmtree(dir_path_docs)
 
     print("Copying static files to public directory...")
-    recursive_copying(dir_path_static, dir_path_public)
+    recursive_copying(dir_path_static, dir_path_docs)
 
     print(
-        f"Generating page from {dir_path_content} to {dir_path_public} using {template_path}."
+        f"Generating page from {dir_path_content} to {dir_path_docs} using {template_path}."
     )
-    recursive_page_generation(dir_path_content, template_path, dir_path_public)
+    recursive_page_generation(
+        dir_path_content, template_path, dir_path_docs, basepath
+    )
 
 
 main()
